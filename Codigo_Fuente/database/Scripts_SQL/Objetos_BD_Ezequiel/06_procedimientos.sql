f88447934531af9fb4df76ee17a94dcfcd3b01ac
Ezequiel Adolfo Herrera Hernández <a385717@uach.mx>
2026-05-27 18:18:01 -0600

    Mis cambios antes de actualizar

--- Codigo_Fuente/database/Scripts_SQL/Objetos_BD_Ezequiel/06_procedimientos.sql
+++ Codigo_Fuente/database/Scripts_SQL/Objetos_BD_Ezequiel/06_procedimientos.sql
@@ -1,6 +1,4 @@
-﻿-- ============================================================
---  BANCO DE ALIMENTOS COMUNITARIO
+﻿--  BANCO DE ALIMENTOS COMUNITARIO
 --  -- 3 Procedimientos Almacenados
 --  Fundamentos de Bases de Datos - Tercer Parcial
--- ============================================================
 
--- Codigo_Fuente/main/main.py
+++ Codigo_Fuente/main/main.py
@@ -3,16 +3,14 @@ from tkinter import messagebox, ttk
 import oracledb
 import sys
 
-# ============================================================================
+
 # 1. CAPA DE DATOS (Configuración y Enlace con Oracle Database 21c)
-# ============================================================================
 DB_USER = "EZEQUIEL"
-DB_PASSWORD = "123789"  # <-- Cambia esto por la contraseña de tu usuario Oracle
+DB_PASSWORD = "123789"  
 DB_DSN = "localhost:1521/XEPDB1"
 
 def conectar_base_datos():
     try:
-        # Se remueve 'thin=True' ya que la versión actual de oracledb lo maneja por defecto
         connection = oracledb.connect(user=DB_USER, password=DB_PASSWORD, dsn=DB_DSN)
         print("[Capa de Datos]: Conexión establecida con éxito en Oracle Database.")
         return connection
